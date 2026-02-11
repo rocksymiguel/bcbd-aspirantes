24c761fb4b00fe27b35a3aa8e38188de4cb5cd59
rocksymiguel <rocksymiguel@hotmail.com>
2026-02-11 12:08:05 -0500

    Add Header on Brigadas

--- js/layout.js
+++ js/layout.js
@@ -1,12 +1,18 @@
-const basePath = window.location.hostname.includes("github.io")
+const isGitHub = window.location.hostname.includes("github.io");
+
+const basePath = isGitHub
   ? "/bcbd-aspirantes/"
   : "/";
 
 fetch(basePath + "partials/header.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("header").innerHTML = data;
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("header").innerHTML = html;
+
+    // Ajustar enlaces automáticamente
+    const links = document.querySelectorAll("#header a");
+    links.forEach(link => {
+      link.href = basePath + link.getAttribute("href");
+    });
   })
-  .catch(error => {
-    console.error("Error cargando header:", error);
-  });
+  .catch(err => console.error("Error cargando header:", err));
