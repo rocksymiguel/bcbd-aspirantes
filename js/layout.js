02aa4e384141b29aa20824fa58aee10fe3bbfc93
rocksymiguel <rocksymiguel@hotmail.com>
2026-02-11 11:35:48 -0500

    Fix missing slash in basePath

--- js/layout.js
+++ js/layout.js
@@ -2,10 +2,10 @@ const basePath = window.location.hostname.includes("github.io")
   ? "/bcbd-aspirantes/"
   : "/";
 
-fetch(basePath + "/partials/header.html")
+fetch(basePath + "partials/header.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("header").innerHTML = html;
-  })
+  })git add js/layout.js
   .catch(err => console.error("Error cargando header:", err));
 
